a6d105fe14c8c73b937635ed385cd788bf5a44b0
Frank <jfrank1500@gmail.com>
2022-06-13 11:00:50 -0300

    Playing with addition and subtraction decimal

---
+++ 20_decimal/README.md
@@ -0,0 +1,12 @@
+## Issues
+
+
+### S0C6 – Specification Exception (Multiplication)
+An address doesn’t specify the boundary required.
+- The multiplier or divisor in a decimal (packed) arithmetic instruction exceeds 15 digits and sign.
+- The first operand field is shorter than or equal to the second operand field in a decimal (packed) multiplication or division instruction.
+
+### S0C7 – Data Exception
+Data was incorrect format for the instruction that attempting to process it. The ABEND can occur when packed decimal instruction are used.
+- The decimal multiplicand has too many high-order significant digits
+- Data fields was not initialized, blanks were read into a field designed to be processed with packed decimal instruction.
--- 20_decimal/main.c
+++ 20_decimal/main.c
@@ -1,22 +1,77 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
-{
-   char *input = "12345678";
-   char *output = "00000000";
+#include <string.h>
 
-   asm("PACK  0(8,%[output]),0(8,%[input])"
-       : /* No output operands */
-       : /* Input operands */
-       [input] "r"(input),
-       [output] "r"(output)
-       : "0", "1", "15");
-   int i;
-   for (i = 0; i < 8; i++)
-   {
-      printf("%.2X ", output[i]);
-   }
-   return 0;
+void pack8(char *output, char *input)
+{
+    asm("PACK  0(7,%[output]),0(8,%[input])"
+        : /* No output operands */
+        : /* Input operands */
+        [input] "r"(input),
+        [output] "r"(output)
+        : "0", "1", "15");
 }
 
-asm("\nINPUT\tDC    CL8'12345678'");
-asm("\nOUTPUT\tDS    PL8");
+void add7(char *output, char *input)
+{
+    asm("AP    0(7,%[output]),0(7,%[input])"
+        : /* No output operands */
+        : /* Input operands */
+        [input] "r"(input),
+        [output] "r"(output)
+        : "0", "1", "15");
+}
+
+void sub7(char *output, char *input)
+{
+    asm("SP    0(7,%[output]),0(7,%[input])"
+        : /* No output operands */
+        : /* Input operands */
+        [input] "r"(input),
+        [output] "r"(output)
+        : "0", "1", "15");
+}
+
+void display(char *name, char *buffer)
+{
+    int i;
+    printf("%s", name);
+    for (i = 0; i < 7; i++)
+    {
+        printf("%.2X", buffer[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+    const char *LZEROS = "00000000";
+    const char *L12345678 = "12345678";
+    const char *L00000011 = "00000011";
+
+    char a[8];
+    char b[8];
+    char pa[7];
+    char pb[7];
+
+    
+    strncpy(a, L12345678, 8);
+    pack8(pa, a);
+    strncpy(b, L00000011, 8);
+    pack8(pb, b);
+
+    printf("\nInitial values\n");
+    display("A=", pa);
+    display("B=", pb);
+
+    add7(pa, pb);
+    printf("\nAfter addition\n");
+    display("A=", pa);
+    display("B=", pb);
+
+    sub7(pa, pb);
+    printf("\nAfter subtraction\n");
+    display("A=", pa);
+    display("B=", pb);
+
+    return 0;
+}
